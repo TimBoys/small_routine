794cec181317f46bc32c67a0f02a99c69f172176
TimBoys <786575400@qq.com>
2018-07-25 10:07:03 +0800

    init sjt

---
+++ sujiangtai/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Rem from '@/assets/js/rem'
+	
+new Rem();
+
+export default {
+  name: 'App',
+  watch:{
+			'$route'(to,from){
+//				console.log(to)
+//				console.log(from)
+				
+			}
+		},
+}
+</script>
+
+<style lang="scss" type="text/scss">
+	@import "./assets/scss/public";
+	@import "./assets/scss/util";
+
+
+</style>
---
+++ sujiangtai/src/main.js
@@ -0,0 +1,49 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store/store'
+import axios from "axios";
+import { XButton,Group,Cell,dateFormat } from 'vux'
+import VueI18n from 'vue-i18n';
+import messages from './i18n/i18n';
+import {localLang} from './i18n/i18n';
+import VueLazyload from 'vue-lazyload';
+
+//配置axios
+//axios.defaults.headers.get['Content-Type'] = 'application/json';
+Vue.prototype.$http = axios;
+
+//配置vux
+Vue.component('x-button', XButton);
+Vue.component('group', Group);
+Vue.component('cell', Cell);
+
+//配置i18n
+Vue.use(VueI18n);
+const i18n = new VueI18n({
+    locale: localLang, // 语言标识
+    messages
+})
+
+//配置懒加载
+Vue.use(VueLazyload)
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: '',
+  loading: '',
+  attempt: 1
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  i18n,
+  store,
+  components: { App },
+  template: '<App/>'
+})
